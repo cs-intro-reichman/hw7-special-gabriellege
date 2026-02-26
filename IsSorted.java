0b8760b0708dba5ebdccf82e0a905c5b30cea61c
Gabrielle Geffen <gabrielle.geffen@post.runi.ac.il>
2026-02-26 18:18:23 +0200

    Completed assignment

--- IsSorted.java
+++ IsSorted.java
@@ -7,15 +7,20 @@ public class IsSorted {
 
     // Helper recursive function
     private static boolean isSorted(int[] a, int i) {
-        //// Replace the following statement with your code
-        return false; 
+        if (i + 1 >= a.length) {
+            return true;
+        }
+        if (a[i] <= a[i + 1]) {
+            return isSorted(a, i + 1);
+        }
+        return false;
     }
 
     public static void main(String[] args) {
-        int[] a = {1, 2, 2, 5};
+        int[] a = { 1, 2, 2, 5 };
         System.out.println(isSorted(a)); // true
 
-        int[] b = {1, 3, 2};
+        int[] b = { 1, 3, 2 };
         System.out.println(isSorted(b)); // false
     }
 }
