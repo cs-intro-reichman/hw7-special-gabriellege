fa6db83fe0f3962005f09de605a0b334cf0664ef
Gabrielle Geffen <gabrielle.geffen@post.runi.ac.il>
2026-02-26 13:55:32 +0200

    Completed assignment

--- Palindrome.java
+++ Palindrome.java
@@ -1,13 +1,15 @@
 /** Checks if a given string is a palindrome. */
 public class Palindrome {
 
-	public static void main(String[]args) {
-    	System.out.println(isPalindrome(args[0]));
-    }
-	
+	public static void main(String[] args) {
+		System.out.println(isPalindrome(args[0]));
+	}
+
 	/** Checks if the given string is a palindrome. */
 	public static boolean isPalindrome(String s) {
-		//// Replace the followaing statement with your code
+		if (s.charAt(0) == s.charAt(-1)) {
+			return isPalindrome(s.substring(1, s.length() - 2));
+		}
 		return false;
-    }
+	}
 }
